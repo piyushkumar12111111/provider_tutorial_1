984fee0051b2ec0de16dd7c6b0e00520d9d680d3
piyushkumar12111111 <103259442+piyushkumar12111111@users.noreply.github.com>
2023-06-11 23:43:00 +0530

    first commit

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<Products>(
+      context,
+      //false to listner will no update them when something will get updated it will be fix
+      listen: false,
+    ).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
